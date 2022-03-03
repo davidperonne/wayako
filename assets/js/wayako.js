2427462381064e86ec82ee6c392e8aa18e31cf72
David PERONNE <david.peronne@wayako.com>
2022-03-03 19:22:32 +0100

    Drago style

--- assets/js/wayako.js
+++ assets/js/wayako.js
@@ -1,15 +1,42 @@
 jQuery(function($) {
 
 
-    // Anti spam test.
-    /*   var form = $('wpcf7-form');
 
-    new Request({
-        url: form.action,
-        method: 'post',
-        onRequest: function() {},
-        onSuccess: function(content) {},
-        onComplete: function() {}
-    }).send(form.toQueryString() + '&is_legit=1');
-*/
+
+    // Show on hover
+    $('ul.main-menu li.dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+    });
+
+    $('ul.main-menu li.shop-dropdown').hover(function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(300);
+    }, function() {
+        $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(300);
+    });
+
+
+
+
+    // To top button
+
+    var offset = 100,
+        speed = 250,
+        duration = 500,
+        scrollButton = $('#topbutton');
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() < offset) {
+            scrollButton.fadeOut(duration);
+        } else {
+            scrollButton.fadeIn(duration);
+        }
+    });
+    scrollButton.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, speed);
+    });
+
 });
--- inc/blocks.php
+++ inc/blocks.php
@@ -90,6 +90,41 @@ if ( ! function_exists( 'wayako_register_acf_block_types' ) ) :
 			)
 		);
 
+		acf_register_block_type(
+			array(
+				'name'            => 'projects_slider',
+				'title'           => __( 'Projects slider', 'wayako' ),
+				'description'     => __( 'Projects slider', 'wayako' ),
+				'render_template' => get_template_directory() . '/blocks/projects-slider.php',
+				'category'        => 'formatting',
+				'icon'            => file_get_contents( get_template_directory() . '/assets/img/picto-w-icon.svg' ),
+				'keywords'        => array( __( 'Project slider', 'Projects', 'Slider' ), __( 'Plugin', 'wayako' ) ),
+			)
+		);
+
+		acf_register_block_type(
+			array(
+				'name'            => 'image_box',
+				'title'           => __( 'Image box', 'wayako' ),
+				'description'     => __( 'Image box', 'wayako' ),
+				'render_template' => get_template_directory() . '/blocks/image-box.php',
+				'category'        => 'formatting',
+				'icon'            => file_get_contents( get_template_directory() . '/assets/img/picto-w-icon.svg' ),
+				'keywords'        => array( __( 'Image box' ), __( 'Plugin', 'wayako' ) ),
+			)
+		);
+
+		acf_register_block_type(
+			array(
+				'name'            => 'testimonials_slider',
+				'title'           => __( 'Testimonials slider', 'wayako' ),
+				'description'     => __( 'Testimonials slider', 'wayako' ),
+				'render_template' => get_template_directory() . '/blocks/testimonials-slider.php',
+				'category'        => 'formatting',
+				'icon'            => file_get_contents( get_template_directory() . '/assets/img/picto-w-icon.svg' ),
+				'keywords'        => array( __( 'Testimonials slider', 'Testimonials', 'Slider' ), __( 'Plugin', 'wayako' ) ),
+			)
+		);
 
 	}
 
