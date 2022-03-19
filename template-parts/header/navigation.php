eeeb9a388950c2d9f1204f415c553c0235097855
David PERONNE <david.peronne@wayako.com>
2022-03-19 08:55:35 +0100

    Optimize theme

--- template-parts/header/navigation.php
+++ template-parts/header/navigation.php
@@ -7,8 +7,8 @@
 
 ?>
 
-<nav id="site-navigation" class="main-navigation">			
-	<button class="menu-toggle" type="button" aria-controls="primary-menu" aria-expanded="false">
+<nav id="main-navigation" class="main-navigation">			
+	<button class="nav-toggle" type="button" aria-controls="primary-menu" aria-expanded="false">
 		<span class="icon-bar top-bar"></span>
 		<span class="icon-bar middle-bar"></span>
 		<span class="icon-bar bottom-bar"></span>
@@ -18,11 +18,12 @@
 	wp_nav_menu(
 		array(
 			'theme_location' => 'primary',
+		//	'container'      => false,
+			'container_class' => 'primary-menu-container site-header__main-nav',
 			'menu_id'        => 'primary-menu',
 			'menu_class'      => 'menu-wrapper main-menu',
-			'container_class' => 'primary-menu-container site-header__main-nav',
 			'fallback_cb'     => false,
 		)
 	);
 	?>
-</nav><!-- #site-navigation -->
+</nav><!-- #main-navigation -->
