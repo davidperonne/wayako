39502e5caee9c5b079b51fbef1646260fb538bfd
David PERONNE <david.peronne@wayako.com>
2022-05-06 19:56:05 +0200

    Css

--- template-parts/header/branding.php
+++ template-parts/header/branding.php
@@ -9,6 +9,6 @@
 
 <div class="site-branding site-header__branding">
 	<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url">
-		<img class="wayako-logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/wayako-logo-2x.png'; ?>" alt="Wayako" width="200" height="60" />
+		<img class="wayako-logo" src="<?php echo get_stylesheet_directory_uri() . '/assets/img/wayako-logo-2x.png'; ?>" alt="Wayako" width="199" height="41.5" />
 	</a>
 </div><!-- .site-branding -->
